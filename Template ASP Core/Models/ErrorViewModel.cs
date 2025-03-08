ca5159bb17e27c32af8fd5ffe8cce3bd2b86ac78
hochitrung8124 <hochitrung08012004@gmail.com>
2025-03-08 09:10:29 +0700

    Project fist

---
+++ Template ASP Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Template_ASP_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
