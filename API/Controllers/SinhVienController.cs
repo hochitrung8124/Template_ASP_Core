3ee9fd32b6bbd0b7c9987357bfa04fe6c40b4df2
hochitrung8124 <hochitrung08012004@gmail.com>
2025-03-11 20:03:13 +0700

    a

--- API/Controllers/SinhVienController.cs
+++ API/Controllers/SinhVienController.cs
@@ -4,29 +4,37 @@ using Data;
 using System.Collections.Generic;
 
 namespace API.Controllers;
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SinhVienController : ControllerBase
-    {
+[Route("api/[controller]")]
+[ApiController]
+public class SinhVienController : ControllerBase
+{
     private static readonly List<SinhVien> List_SinhVien = new List<SinhVien>()
     {
         new SinhVien(){id = 01, name = "Trung"},
-        new SinhVien(){id = 01, name = "Trung2"},
-        new SinhVien(){id = 01, name = "Trung3"},
-        new SinhVien(){id = 01, name = "Trung4"},
-        new SinhVien(){id = 01, name = "Trung5"},
-        new SinhVien(){id = 01, name = "Trung6"},
-        new SinhVien(){id = 01, name = "Trung7"}
+        new SinhVien(){id = 02, name = "Trung2"},
+        new SinhVien(){id = 03, name = "Trung3"},
+        new SinhVien(){id = 04, name = "Trung4"},
+        new SinhVien(){id = 05, name = "Trung5"},
+        new SinhVien(){id = 06, name = "Trung6"},
+        new SinhVien(){id = 07, name = "Trung7"}
     };
-/*    [HttpGet]
-        public IEnumerable<SinhVien> GetAll() 
-        { 
-            return (IEnumerable<SinhVien>)Ok(List_SinhVien);
-        }*/
+    /*    [HttpGet]
+            public IEnumerable<SinhVien> GetAll() 
+            { 
+                return (IEnumerable<SinhVien>)Ok(List_SinhVien);
+            }*/
     [HttpGet]
     public IEnumerable<SinhVien> GetAll()
     {
         return List_SinhVien;
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var thongTin = List_SinhVien.SingleOrDefault(tt => tt.id.Equals(id));
+        if (thongTin == null)
+              return BadRequest("không có dữ liệu");
+        return Ok(thongTin);     
     }
 }
 
--- Template ASP Core/Controllers/HomeController.cs
+++ Template ASP Core/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
-
+﻿
 using Data;
+using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Xml;
+using System.Xml.Linq;
 using Template_ASP_Core.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Template_ASP_Core.Controllers
 {
@@ -24,20 +28,28 @@ namespace Template_ASP_Core.Controllers
             var dataJson = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(res);
             return View(dataJson);
         }
-
         public async Task<IActionResult> Privacy()
         {
             HttpClient client = new HttpClient();
-            var data = await client.GetAsync("https://localhost:7155/api/SinhVien");
+            var data = await client.GetAsync($"https://localhost:7155/api/SinhVien");
             var res = await data.Content.ReadAsStringAsync();
             var dataJson = JsonConvert.DeserializeObject<IEnumerable<SinhVien>>(res);
             return View(dataJson);
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public async Task<IActionResult> GetById(int name)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            HttpClient client = new HttpClient();
+            var data = await client.GetAsync($"https://localhost:7155/api/SinhVien/{name}");
+            var res = await data.Content.ReadAsStringAsync();
+            var dataJson = JsonConvert.DeserializeObject<SinhVien>(res);
+
+            return View(dataJson);
         }
-    }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+        }
 }
